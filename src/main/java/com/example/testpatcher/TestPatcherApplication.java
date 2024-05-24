b572afdef240cf789ac5f1e9a678eace900e1ec9
waelhassanmu-dev <waelhassanmu-dev@gmail.com>
2024-05-24 15:49:53 +0400

    first commit

---
+++ src/main/java/com/example/testpatcher/TestPatcherApplication.java
@@ -0,0 +1,13 @@
+package com.example.testpatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestPatcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testpatcher/TestPatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testpatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
